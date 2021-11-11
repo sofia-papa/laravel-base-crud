1f2a8859f85b0418fef828faf60c4ae688849344
sofia-papa <papa.sofia@outlook.it>
2021-11-11 15:02:12 +0000

    corrections

---
+++ database/migrations/2021_11_11_111428_create_fumettis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFumettisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fumettis', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('author', 50);
+            $table->text('description');
+            $table->text('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fumettis');
+    }
+}
